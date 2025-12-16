90aafd5f4ffd875fde9f438cebda4bc5f178b51d
Akash <vangadaakash529@gmail.com>
2025-12-16 20:19:36 +0530

    updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,6 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_URL ||
   "http://localhost:5000";
+export const AI_BASE_URL =
+  process.env.REACT_APP_AI_URL ||
+  "http://localhost:8000";
